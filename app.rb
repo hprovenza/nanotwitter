370932c6be6779f833258391dc7e8362604bfd9e
Yuzhe <yzchen@brandeis.edu>
2016-09-28 23:29:46 -0400

    added basic authentication

--- app.rb
+++ app.rb
@@ -1,6 +1,28 @@
 require 'sinatra'
 require 'sinatra/activerecord'
-require './config/environments'
+require './config/environments' 
+require './models/user'
+
+# might need to implement sessions?
+helpers do
+  def protected!
+    return if authorized?
+    headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+    halt 401, "Not authorized\n"
+  end
+
+  def authorized?
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    # first try, would probably need fix
+    if @auth.provided? && @auth.basic? && @auth.credentials
+      # need migration first
+      user = User.find_by(name: @auth.credentials[0], password: @auth.credentials[1])
+      return !user.nil?
+    else
+      return false
+    end
+  end
+end
 
 get '/' do
   erb :index
@@ -12,4 +34,13 @@ end
 
 get '/user/register' do
   erb :register
+end
+
+# routes that need authorization
+get '/home' do
+  if authorized?
+    erb :home
+  else
+    redirect '/'
+  end
 end
