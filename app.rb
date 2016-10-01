f06b8103267a136a37e08a517b6dfd663aa4ed56
signalw <xinhao@brandeis.edu>
2016-09-30 22:24:29 -0400

    Fixed login settings

--- app.rb
+++ app.rb
@@ -9,56 +9,50 @@ get '/' do
   erb :index
 end
 
-get '/user/register' do
-  erb :register, :locals=>{:message => nil}
-end
-
-
-post '/user/register' do
-  @user = User.new(params[:user])
-  @existing = User.where("username = ?", params[:user][:username])
-  if @existing.count != 0
-    erb :register, :locals=>{:message =>
-      "Account already exists! Try a new username or log back in!"}
-  elsif @user.save
-    redirect '/user/register/success'
-  else
-    "Sorry our server died..."
-  end
-end
-
-get '/user/register/success' do
-  erb :register_success
-end
-
 get '/login' do
   erb :login
 end
 
 post '/login/submit' do
-  user = User.find_by(email: params["email"], password: params["password"])
-  if user.nil?
-    redirect '/'
+  @user = User.find_by(username: params[:user][:username], password: params[:user][:password])
+  if @user.nil?
+    "Either username does not exist or wrong password"
   else
-    session[:id] = user.id
+    session[:id] = @user.id
     redirect '/home'
   end
+end
+
+get '/user/register' do
+  erb :register, :locals=>{:message => nil}
 end
 
 # routes that need authorization
 get '/home' do
   if session[:id].nil?
-    redirect '/login'
+    redirect '/'
   end
   @user = User.find(session[:id])
   if @user.nil?
-    redirect '/login'
+    redirect '/'
   else
     erb :home
   end
 end
 
-get '/logout' do
-  session.clear
-  erb '/'
+post '/user/register' do
+  @user = User.new(params[:user])
+  @existing = User.find_by("username = ?", params[:user][:username])
+  if !@existing.nil?
+    erb :register, :locals=>{:message =>
+      "Account already exists! Try a new username or log back in!"}
+  elsif @user.save
+    redirect '/user/register/success'
+  else
+    "Sorry our server is taking a nap"
+  end
+end
+
+get '/user/register/success' do
+  erb :register_success
 end
