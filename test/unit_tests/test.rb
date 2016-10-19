404409f1541ba9abdc7bcc30da6142bafd6dd3e5
Yuzhe <yzchen@brandeis.edu>
2016-10-18 20:45:44 -0400

    basic unit tests (need improvement)

--- test/unit_tests/test.rb
+++ test/unit_tests/test.rb
@@ -7,8 +7,32 @@ class NTTest < MiniTest::Unit::TestCase
     Sinatra::Application
   end
 
+  def setup
+    get '/test/reset/all'
+  end
+
   def test_main_page
     get '/'
+    assert last_response.ok?
+  end
+
+  def test_user_number
+    assert User.all.size == 1
+  end
+
+  def test_register
+    get '/user/register'
+    assert last_response.ok?
+    post '/user/register', {:user=>{:username=>'testuser', :password=> 'password'}}
+    assert User.all.size == 1
+    post '/user/register', {:user=>{:username=>'test', :password=> 'test'}}
+    assert User.all.size == 2
+  end
+
+  def test_login
+    get '/login'
+    assert last_response.ok?
+    post '/login', {:user=>{:username=>'test', :password=> 'test'}}
     assert last_response.ok?
   end
 end
