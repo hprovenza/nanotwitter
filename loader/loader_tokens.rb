c80b14e48a8e0a7d70842f8b3e18b07a91bb511b
Yuzhe <yzchen@brandeis.edu>
2016-11-04 16:32:38 -0400

    add loader token

--- loader/loader_tokens.rb
+++ loader/loader_tokens.rb
@@ -1,3 +1,7 @@
 get '/loaderio-85a2c506cdce13988be053d2e7b28c95/' do
   "loaderio-85a2c506cdce13988be053d2e7b28c95"
 end
+
+get '/loaderio-6051eff7d0027b4ef27e6eacbfd6e4bd' do
+  "loaderio-6051eff7d0027b4ef27e6eacbfd6e4bd"
+end
