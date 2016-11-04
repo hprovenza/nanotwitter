bbad2404a6f3deae91a368c01a7b0a2b7589ec07
Yuzhe <yzchen@brandeis.edu>
2016-11-04 16:36:19 -0400

    fix URL

--- loader/loader_tokens.rb
+++ loader/loader_tokens.rb
@@ -2,6 +2,6 @@ get '/loaderio-85a2c506cdce13988be053d2e7b28c95/' do
   "loaderio-85a2c506cdce13988be053d2e7b28c95"
 end
 
-get '/loaderio-6051eff7d0027b4ef27e6eacbfd6e4bd' do
+get '/loaderio-6051eff7d0027b4ef27e6eacbfd6e4bd/' do
   "loaderio-6051eff7d0027b4ef27e6eacbfd6e4bd"
 end
