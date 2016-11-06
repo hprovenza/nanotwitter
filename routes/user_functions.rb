0c86dfa3dfd56efcf39f55b517d1a8435d86161c
Hannah Provenza <hprovenza@gmail.com>
2016-11-06 12:48:13 -0500

    changed method to more closely match example 2

--- routes/user_functions.rb
+++ routes/user_functions.rb
@@ -118,18 +118,18 @@ end
 
 post '/update_pic' do
   @user = User.find(session[:id])
-  name = @user.name
   file       = params[:file][:tempfile]
+  filename   = params[:file][:filename]
   AWS::S3::Base.establish_connection(
       :access_key_id     => s3_key,
       :secret_access_key => s3_secret
   )
   AWS::S3::S3Object.store(
-      name,
+      filename,
       open(file.path),
       s3_bucket,
       :access => :public_read
   )
   url = "https://#{s3_bucket}.s3.amazonaws.com/#{name}"
-  erb :settings
+  return url
 end
