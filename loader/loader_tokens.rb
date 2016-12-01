15e3670167d6ff534e5a5882948a5e194aaa0fe2
Hannah Provenza <hprovenza@gmail.com>
2016-12-01 14:02:53 -0500

    added loader token for gitpage report

--- loader/loader_tokens.rb
+++ loader/loader_tokens.rb
@@ -9,3 +9,7 @@ end
 get '/loaderio-25df39ce112b20296dd11c21d6003f2a/' do
   "loaderio-25df39ce112b20296dd11c21d6003f2a"
 end
+
+get '/loaderio-1e97e3465d2c2ba323d4286a2a709bbb' do
+  "loaderio-1e97e3465d2c2ba323d4286a2a709bbb"
+end
