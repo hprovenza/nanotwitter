1f80ddd687ddf2787d7fbadd3b6eca0b53fcee9d
Hannah Provenza <hprovenza@gmail.com>
2016-12-01 15:46:01 -0500

    forgot a slash on load tester

--- loader/loader_tokens.rb
+++ loader/loader_tokens.rb
@@ -10,6 +10,6 @@ get '/loaderio-25df39ce112b20296dd11c21d6003f2a/' do
   "loaderio-25df39ce112b20296dd11c21d6003f2a"
 end
 
-get '/loaderio-1e97e3465d2c2ba323d4286a2a709bbb' do
+get '/loaderio-1e97e3465d2c2ba323d4286a2a709bbb/' do
   "loaderio-1e97e3465d2c2ba323d4286a2a709bbb"
 end
